796f59229e417975101651bf4e19615a0ed314d5
Maiwand Zazai <Maiwand.Zazai77@gmail.com>
2025-10-06 01:42:39 +0200

    showing and closing the modal if any fo 3 buttons cliscked

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const modalWindow = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const showBTN = document.querySelectorAll('.show-modal');
+const closeModal = document.querySelector('.close-modal');
+const bodyElement = document.querySelector('body');
+
+for (let show of showBTN) {
+  show.addEventListener('click', () => {
+    modalWindow.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+closeModal.addEventListener('click', () => {
+  modalWindow.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
